1973f0637edaf2dfbc8a99b222961b8b1995da64
Nacho Morales <nacho.morales2016@gmail.com>
2021-10-12 19:06:24 -0300

    Tercer Commit

--- src/libreria/entidades/Cliente.java
+++ src/libreria/entidades/Cliente.java
@@ -24,6 +24,7 @@ public class Cliente implements Serializable {
     private String apellido;
     private String telefono;
     private String apodo;
+    private String apodo2;
     public Cliente() {
     }
 
